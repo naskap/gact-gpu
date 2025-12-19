b4809e172763ec2cce934a785959023f0e8df9fe
Rena Feng <158506274+rf5447@users.noreply.github.com>
2025-12-19 10:38:48 -0500

    Update dc_gact.tcl

--- synthesis/dc_gact.tcl
+++ synthesis/dc_gact.tcl
@@ -1,5 +1,6 @@
 # ===============================
 # dc_gact.tcl
+# used Google Gemini to help generate scripts and modified it to our needs: https://gemini.google.com/share/313d2433cab2
 # Optimized for hierarchical SV
 # ===============================
 
